5a5d137b075a0d79670bba333ca66cc6452b7936
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2022-07-17 17:08:54 -0300

    DB Models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,29 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING, // String para usar un rango. EJ: "1 - 5"
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING, // String para usar un rango. EJ: "1 - 5"
+      allowNull: false
+    },
+    lifespan: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, 
+  {
+    timestamps: false,
   });
 };
