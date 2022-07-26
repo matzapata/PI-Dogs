fbe240e116c895f6a83aa306c876251e19207bd5
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2022-07-26 15:10:04 -0300

    index.html updates and sync temperaments table in syncDbUtil

--- api/utils/syncDb.js
+++ api/utils/syncDb.js
@@ -2,6 +2,7 @@ const { conn } = require('../src/db.js');
 
 console.log("Remember to update .env DB_URI to match the DB that you want to synchronize")
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+    await require('./populateTemperaments')(); // Populate temperaments db
     console.log("DB synced")
 })
