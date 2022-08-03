144c2d2ac5732215df47476fcd7758374f93c146
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2022-08-03 17:50:13 -0300

    Pagination function for reducer readability

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -42,36 +42,40 @@ export const initialState = {
     temperaments: []
 };
 
+const pagination = (dogs, pageNumber, pageSize = 8) => {
+    const totalPages = Math.ceil(dogs.length / pageSize);
+    return {
+        next: (pageNumber === totalPages || totalPages === 1) ? null : pageNumber + 1,
+        prev: (pageNumber - 1) ? pageNumber - 1 : null,
+        current: pageNumber,
+        total: totalPages,
+        pageSize: pageSize,
+        pageContent: dogs.slice(
+            (pageNumber - 1) * pageSize
+            ,
+            (pageNumber) * pageSize
+        )
+    };
+};
+
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SET_DOGS: {
-            const totalPages = Math.ceil(action.payload.length / state.pagination.pageSize);
-
+        case SET_DOGS: 
             return {
                 ...state,
                 dogs: action.payload,
                 filteredDogs: action.payload,
-                pagination: {
-                    next: (totalPages === 1) ? null : 2,
-                    prev: null,
-                    current: 1,
-                    total: totalPages,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: action.payload.slice(0, state.pagination.pageSize)
-                }
+                pagination: pagination(action.payload, 1)
             };
-        }
-        case SET_SEARCH: 
+        case SET_SEARCH:
             return {
                 ...state,
                 search: action.payload
-            }
+            };
         case SET_DOG_DETAIL:
             return {
                 ...state,
-                dogDetail: {
-                    ...action.payload
-                }
+                dogDetail: action.payload
             };
         case SET_TEMPERAMENTS:
             return {
@@ -81,49 +85,17 @@ const rootReducer = (state = initialState, action) => {
         case NEXT_PAGE:
             return {
                 ...state,
-                pagination: {
-                    next: (state.pagination.current + 1 === state.pagination.total) ? null : state.pagination.current + 2,
-                    prev: state.pagination.current,
-                    current: state.pagination.current + 1,
-                    total: state.pagination.total,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: state.filteredDogs.slice(
-                        (state.pagination.current) * state.pagination.pageSize
-                        ,
-                        (state.pagination.current + 1) * state.pagination.pageSize
-                    )
-                }
+                pagination: pagination(state.filteredDogs, state.pagination.next)
             };
         case PREV_PAGE:
             return {
                 ...state,
-                pagination: {
-                    ...state.pagination,
-                    next: state.pagination.current,
-                    prev: (state.pagination.current - 1 === 1) ? null : state.pagination.current - 2,
-                    current: state.pagination.current - 1,
-                    pageContent: state.filteredDogs.slice(
-                        (state.pagination.current - 2) * state.pagination.pageSize
-                        ,
-                        (state.pagination.current - 1) * state.pagination.pageSize
-                    )
-                }
+                pagination: pagination(state.filteredDogs, state.pagination.prev)
             };
         case SET_PAGE:
             return {
                 ...state,
-                pagination: {
-                    next: (action.payload === state.pagination.total) ? null : action.payload + 1,
-                    prev: action.payload - 1,
-                    current: action.payload,
-                    total: state.pagination.total,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: state.filteredDogs.slice(
-                        (action.payload - 1) * state.pagination.pageSize
-                        ,
-                        (action.payload) * state.pagination.pageSize
-                    )
-                }
+                pagination: pagination(state.filteredDogs, action.payload)
             };
         case SORT_DOGS: {
             const sortFilteredDogs = [...state.filteredDogs].sort(action.payload);
@@ -132,19 +104,11 @@ const rootReducer = (state = initialState, action) => {
                 ...state,
                 dogs: [...state.dogs].sort(action.payload),
                 filteredDogs: sortFilteredDogs,
-                pagination: {
-                    next: (state.pagination.total === 1) ? null : 2,
-                    prev: null,
-                    current: 1,
-                    total: state.pagination.total,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: sortFilteredDogs.slice(0, state.pagination.pageSize)
-                }
+                pagination: pagination(sortFilteredDogs, 1)
             };
         }
         case FILTER_DOGS_TEMPERAMENT: {
             const filteredDogs = applyFilters(state.dogs, [filterByOrigin(state.filters.origin), filterByTemperament(action.payload)]);
-            const totalPages = Math.ceil(filteredDogs.length / state.pagination.pageSize);
 
             return {
                 ...state,
@@ -153,19 +117,11 @@ const rootReducer = (state = initialState, action) => {
                     ...state.filters,
                     temperament: action.payload
                 },
-                pagination: {
-                    next: (totalPages === 1) ? null : 2,
-                    prev: null,
-                    current: 1,
-                    total: totalPages,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: filteredDogs.slice(0, state.pagination.pageSize)
-                }
+                pagination: pagination(filteredDogs, 1)
             };
         }
         case FILTER_DOGS_ORIGIN: {
             const filteredDogs = applyFilters(state.dogs, [filterByOrigin(action.payload), filterByTemperament(state.filters.temperament)]);
-            const totalPages = Math.ceil(filteredDogs.length / state.pagination.pageSize);
 
             return {
                 ...state,
@@ -174,21 +130,14 @@ const rootReducer = (state = initialState, action) => {
                     ...state.filters,
                     origin: action.payload
                 },
-                pagination: {
-                    next: (totalPages === 1) ? null : 2,
-                    prev: null,
-                    current: 1,
-                    total: totalPages,
-                    pageSize: state.pagination.pageSize,
-                    pageContent: filteredDogs.slice(0, state.pagination.pageSize)
-                }
+                pagination: pagination(filteredDogs, 1)
             };
         }
         case SET_LOADING:
             return {
                 ...state,
                 loading: action.payload
-            }
+            };
         default:
             return { ...state };
     }
