a73d9f2ee2edb5816f7f6f5380ce1fdcbb3f8964
Matias Zapata <matuzapata@gmail.com>
2022-09-13 20:51:16 -0300

    fix deploy model

--- api/src/db.js
+++ api/src/db.js
@@ -1,27 +1,18 @@
 require('dotenv').config();
 const { Sequelize, Op } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
 const pg = require("pg")
+const dogModel = require("./models/Dog")
+const temperamentModel = require("./models/Temperament")
 
 const sequelize = new Sequelize(process.env.DB_URI, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectModule: pg
 });
-const basename = path.basename(__filename);
 
-const modelDefiners = [];
+dogModel(sequelize);
+temperamentModel(sequelize);
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
-
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
